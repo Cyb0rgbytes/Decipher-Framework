c0fce9161450e1ec5d9e83e3fc32ab0cc623139e
Omar Al Chanawani <34769900+SoftAddict@users.noreply.github.com>
2020-08-16 14:02:54 -0400

    Update Decipher-Framework.py

--- Decipher-Framework.py
+++ Decipher-Framework.py
@@ -20,20 +20,23 @@ def operation():
     operation_options = ['1', '2', '3']
     if operation not in operation_options:
         print (f'\n{Fore.WHITE} Please Enter a Number from the List!')
-        operation()
+    operation_base64()
 
     ########################################
     # Base64
 def operation_base64():
-    if operation == '1':
+    if operation == '1': operation_base64()
         print(f'''{Fore.RED}Do You want to Encrypt or Decrypt?
         E for Encrypt
         D for Decrypt''')
         operation_base64 = input('Operation: ')
-        
+
         operation_base64_options = ['E', 'e', 'D', 'd']
-        if operation_base64 not in operation_base64_options:
-            print (f'\n{Fore.WHITE} Please Enter a letter from the list!')
-    
+    if  operation_base64 not in operation_base64_options:
+        print (f'\n{Fore.WHITE} Please Enter a letter from the list!')
+
+
+        operation_base64()
     operation()
 welcome()
+
