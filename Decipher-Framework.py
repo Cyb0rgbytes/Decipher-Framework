b66d933db4c8425fff5b49da755d740db2b576be
Omar Al Chanawani <34769900+SoftAddict@users.noreply.github.com>
2020-08-16 11:54:03 -0400

    First initiate

---
+++ Decipher-Framework.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3 
+
+import colorama
+from colorama import init, Fore
+import base64
+
+
+def welcome():
+    print(f'{Fore.WHITE}\nWelcome to Decipher Framework {Fore.RED} Follow the instructions to navigate the script')
+    operation()
+
+def operation():
+    print(f'''{Fore.RED}
+    Please select the cipher you want to use
+    1 for Base64
+    2 for Caesar Cipher
+    3 for ROT13''')
+    operation = input('Operation: ')
+
+    operation_options = ['1', '2', '3']
+    if operation not in operation_options:
+        print (f'\n{Fore.WHITE} Please Enter a Number from the List!')
+        operation()
+
+    ########################################
+    # Base64
+def operation_base64():
+    if operation == '1':
+        print(f'''{Fore.RED}Do You want to Encrypt or Decrypt?
+        E for Encrypt
+        D for Decrypt''')
+        operation_base64 = input('Operation: ')
+        
+        operation_base64_options = ['E', 'e', 'D', 'd']
+        if operation_base64 not in operation_base64_options:
+            print (f'\n{Fore.WHITE} Please Enter a letter from the list!')
+    
+    operation()
+welcome()
