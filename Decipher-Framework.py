72226c538d44afedc6dc77a9d6f90878f7d80f34
Sedatyf <gaxel@hotmail.fr>
2020-08-29 16:00:08 +0200

    Added file feature for base64 cipher function

--- Decipher-Framework.py
+++ Decipher-Framework.py
@@ -7,6 +7,14 @@ from colorama import Fore, Back
 def welcome():
     print(f'{Fore.WHITE}\nWelcome to Decipher Framework {Fore.RED} Follow the instructions to navigate the script')
 
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+    return content
+
+def write_file(file_path, content):
+    with open(file_path, 'w') as f:
+        f.write(content)
 
 def decipher():
     print(f'''{Fore.RED}
@@ -33,14 +41,36 @@ def decipher_base64():
   E for Encrypt
   D for Decrypt''')
     user_input = input('Operation: ').lower()
+
+    if user_input == 'e':
+        print(f'''{Fore.RED}Do you want to encrypt a file?
+        Y for Yes
+        N for No''')
+    else:
+        print(f'''{Fore.RED}Do you want to decrypt a file?
+        Y for Yes
+        N for No''')
+    file_choice = input('Operation: ').lower()
+
+    if file_choice == 'y':
+        file_path = input("What is the path of the file? ")
+        file_content = read_file(file_path)
+    else:
+        print('Please enter the string you would like Base64 encoded')
+        string_input = input('String: ')
+
     if user_input in options:
-        if user_input == 'e':
-            print('Please enter the string you would like Base64 encoded')
-            string_input = input('String: ')
-            print(f'\n{Fore.WHITE}our Ecrypted Text >>> {base64.b64encode(string_input.encode()).decode()}')
+        if user_input == 'e' and file_choice == 'y':
+            encoded_string = base64.b64encode(file_content.encode()).decode()
+            print(f'\n{Fore.WHITE}Your Ecrypted Text >>> {encoded_string}')
+            write_file(file_path, encoded_string)
+        elif user_input == 'd' and file_choice == 'y':
+            encoded_string = base64.b64decode(file_content.encode()).decode()
+            print(f'\n{Fore.WHITE}Your Ecrypted Text >>> {encoded_string}')
+            write_file(file_path, encoded_string)
+        elif user_input == 'e':
+            print(f'\n{Fore.WHITE}Your Ecrypted Text >>> {base64.b64encode(string_input.encode()).decode()}')
         elif user_input == 'd':
-            print('Please enter the string you would like Base64 decoded')
-            string_input = input('String: ')
             print(f'\n{Fore.WHITE}Your Decrypted Text >>> {base64.b64decode(string_input.encode()).decode()}')
     else:
         print(f'\n{Fore.WHITE} Please Enter a letter from the list!')
